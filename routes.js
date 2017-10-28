f0ffe81c8621be294fd7a57f1e5977e976613a21
arthur <arthurkishinets@gmail.com>
2017-10-28 22:26:06 +0300

    fixes

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-router.get('/api/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
-router.get('/api/auth/facebook/callback', passport.authenticate('facebook',
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+router.get('/auth/facebook/callback', passport.authenticate('facebook',
   { successRedirect: '/main', failureRedirect: '/auth' }));
 
 module.exports = router;
